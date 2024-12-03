a93a86000b14a8bc223ce3a9dbd0844c28195f22
azexmon56 <102878815+azexmon56@users.noreply.github.com>
2024-12-03 11:17:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
